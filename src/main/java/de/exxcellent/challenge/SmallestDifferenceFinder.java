b44fb1173ee10cda07e7e5255bc769e69767c16e
MalikS <sammar.malik@accenture.com>
2017-10-04 14:13:51 +0100

    Process csv file and output the day with the smallest temperature spread

--- src/main/java/de/exxcellent/challenge/SmallestDifferenceFinder.java
+++ src/main/java/de/exxcellent/challenge/SmallestDifferenceFinder.java
@@ -1,15 +1,39 @@
 package de.exxcellent.challenge;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.URL;
+import java.util.Arrays;
+
 public final class SmallestDifferenceFinder {
 
-    public static void main(String... args) {
+  public static void main(String... args) {
+    WeatherData weatherData = new WeatherData("MxT", "MnT");
+    processFile(weatherData, getFileURL("de/exxcellent/challenge/weather.csv"));
+    System.out.printf("Day with smallest temperature spread : %s%n", weatherData.getItemWithSmallestDifference());
+  }
 
-        // Your preparation code …
-
-        String dayWithSmallestTempSpread = "Someday";     // Your day analysis function call …
-        String teamWithSmallesGoalSpread = "A good team"; // Your goal analysis function call …
-
-        System.out.printf("Day with smallest temperature spread : %s%n", dayWithSmallestTempSpread);
-        System.out.printf("Team with smallest goal spread       : %s%n", teamWithSmallesGoalSpread);
+  private static URL getFileURL(String fileName) {
+    ClassLoader classLoader = ClassLoader.getSystemClassLoader();
+    URL fileURL = classLoader.getResource(fileName);
+    if(fileURL == null) {
+      System.out.printf("File: %s not found", fileName);
+      System.exit(1);
     }
+    return fileURL;
+  }
+
+  private static void processFile(WeatherData weatherData, URL url) {
+    try {
+      BufferedReader reader = new BufferedReader(new InputStreamReader(url.openStream()));
+      String line;
+      while((line = reader.readLine()) != null) {
+        weatherData.processRow(Arrays.asList(line.split(",")));
+      }
+    } catch(IOException e) {
+      e.printStackTrace();
+    }
+  }
+
 }
